81631cf193de4634623ec0b8d7c6a971948b410d
Romulo Scampini <romulo@scampini.com.br>
2019-02-27 02:21:24 -0300

    feat: wizard to execute run.sh and update readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Ansible-proxy
+Ansible Project to install a proxy server
+
+## Requirements
+ * Ansible
+ * Python
+ * SSH
+ * Git
+
+## Usage
+
+1. Clone this repo
+2. Initialize sub-modules:
+    * Inside dir of cloned repo, run:  
+        *   `git submodules update --init`
+3. Run script  
+    * `./run.sh`
--- run.sh
+++ run.sh
@@ -6,6 +6,19 @@ echo -e '\t3. UAT (Disabled)'
 echo -e '\t4. PRD'
 echo -e '\t0. Sair'
 
+function checkProdServer(){
+    HOST=`cat inventory-prd | grep \<SERVER_HOST\>`
+    USER_SSH=`cat inventory-prd | grep \<USERNAME\>`
+    if [ ! -z "$HOST" ] || [ ! -z "$USER_SSH" ]; then
+        echo -e '\n[ERROR] Dados do servidor de produção não encontrados'
+        echo -e 'Informe os dados do servidor de produção:'
+        printf '\t1. IP ou Host: ' && read -p "" SERVER_HOST
+        printf '\t1. Usuario SSH: ' && read -p "" USERNAME_SSH
+        sed -i '' "s/\<SERVER_HOST\>/${SERVER_HOST}/g" inventory-prd
+        sed -i '' "s/\<USERNAME\>/${USERNAME_SSH}/g" inventory-prd
+    fi
+}
+
 while true; do
     read -p "Digite uma das opções acima: " option
     case $option in
@@ -13,6 +26,7 @@ while true; do
             ansible-playbook -i inventory-local -vv --ask-sudo-pass --extra-vars="@extra-vars.yml" install.yml
             break;;
         4)
+            checkProdServer
             ansible-playbook -i inventory-prd -vv --ask-sudo-pass --extra-vars="@extra-vars.yml" install.yml
             break;;
         0)
